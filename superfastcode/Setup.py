c7cfdb8effef2e193a65edcadac7ea1468177744
cailenR <cailen01@gmail.com>
2019-04-08 14:53:34 +1000

    Updated Name for cpp project and additional includes and libraries needed

--- superfastcode/Setup.py
+++ superfastcode/Setup.py
@@ -6,14 +6,14 @@ from distutils import sysconfig
 cpp_args = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7']
 
 sfc_module = Extension(
-    'superfastcode', sources = ['module.cpp'],
+    'DataStreamerCpp', sources = ['module.cpp'],
     include_dirs=['pybind11/include'],
     language='c++',
     extra_compile_args = cpp_args,
     )
 
 setup(
-    name = 'superfastcode',
+    name = 'DataStreamerCpp',
     version = '1.0',
     description = 'Python package with superfastcode2 C++ extension (PyBind11)',
     ext_modules = [sfc_module],
