1dcf1d15361b2bd2817d96262e40986cc279e00b
cailenR <cailen01@gmail.com>
2019-05-22 15:05:25 +1000

    Changed cppprocessor to be stored in a class wrapper, added a main file to use wrapper.

---
+++ DataStreamer/Main.py
@@ -0,0 +1,183 @@
+
+from itertools import islice
+from random import random
+import time
+import csv
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+from matplotlib import interactive
+import datetime as dt
+import matplotlib.animation as animation
+from pylive import live_plotter
+interactive(True)
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib
+
+import config
+import DataStreamer as DS   # the wrapper for the datastreamer
+
+
+ds = DS.DataStreamer()
+
+
+#update the graph
+def update_line(hl, new_data):
+    hl.set_xdata(numpy.append(hl.get_xdata(), new_data))
+    hl.set_ydata(numpy.append(hl.get_ydata(), new_data))
+    plt.draw()
+
+
+def saveResults(data, filename=None):
+    if filename == None:
+        filename = time.strftime("%Y%m%d-%H%M%S") + '.csv'
+    data.to_csv(config.resultsFilePath+filename, index=False)
+    return 0
+
+
+def caclulateErr(results):
+    df =pd.DataFrame()
+    df["result"] = results["predicted"].str.strip("[]")
+    df["truth"] = results["Label"]
+    df['result'] = df['result'].astype(np.float64)
+    df['truth'] = df['truth'].astype(np.float64)
+    res =df.loc[~(df['result'] == df['truth'])]
+    print("error rate: {}%".format(len(res)/len(results)*100))
+    return 0 
+
+# a better working implementation of the fit transform function available in the label encoder library. Adds in the features
+# I personally expected in it when I used it.
+#performs individual column label encoding. provides list of label encoders for reversing of the transformation at a later date.
+#copy is performed to not effect the original data passed in.
+#object only is the default state, set this to false to enable the label encode for all columns individually.
+def fit_transform_cols(data, object_only = True):
+    if object_only == True:
+        object_data =  data.select_dtypes(include=['object']).copy()
+    else:
+        object_data =  data.copy()
+    # print(types.head())
+    output = pd.DataFrame(data).copy()
+    le_list = {}
+    for col in object_data:   
+        #print(np.sort(object_data[col].unique()) )
+        le = preprocessing.LabelEncoder()
+        le.fit(object_data[col])    
+        le_list[col] = le
+        #print("classes of {} are:{}".format(col,np.sort(le.classes_)))        
+        output[col] = le.transform(object_data[col])
+    return output, le_list
+    
+   #reverses the fit_transform_cols. takes in the dataset and the dictionary of of the label encoders. 
+   # keys of the dictionary refer to the column names of the input dataframe that are to be transformed reversed.
+   #can take single label encoder to run on entire dataframe.
+def transform_reverse_cols(data, le_list):
+    output = data.copy()
+    # if single label encoder is passed, use that for the entire dataframe.
+    if isinstance(le_list,preprocessing.LabelEncoder):
+          for col in output:
+            output[col]= le_list.inverse_transform(output[col])  
+            #print("classes of {} are:{}".format(key,np.sort(value.classes_)))                       
+    else:
+        output = pd.DataFrame(data).copy()
+        for key, value in le_list.items():
+            print("value type:{}".format(type(value)))
+            print("classes of {} are:{}".format(key,np.sort(value.classes_)))
+            #print(key, type(value)) 
+            output[key]= value.inverse_transform(data[key])     
+    return output
+
+def startDataStream():   
+    count = 0
+    inputFile =[]
+    labels =[]
+    csv_start_time = time.monotonic()                           
+    data = pd.read_csv(config.CSVfileName, header = config.HEADER, nrows = config.MAXROWS)
+
+    labels = (data.iloc[:,41])        
+    inputFile =data.drop([41], axis=1)                                        
+    le = preprocessing.LabelEncoder()
+    le.fit(labels)
+    print("classes of labels are:{}".format(le.classes_))
+    labels_encoded = le.transform(labels)
+    
+    inputFile, le_list = fit_transform_cols(inputFile)
+    #inputFile = inputFile.apply(preprocessing.LabelEncoder().fit_transform)
+    #print(inputFile)
+    inputFile = inputFile.astype(str)
+    inputFile = inputFile.values.tolist()
+    labels = labels_encoded.astype(str)
+
+    X_train, X_test, y_train, y_test = train_test_split(inputFile, labels, test_size=config.TESTSIZE, random_state=config.RANDOMSTATE)
+
+    #print(type(labels))                     
+    labels = labels.tolist()
+    if config.DEBUG:
+        print("total set size:{}".format(len(inputFile)))
+        print("train size: {}".format(len(X_train)))            
+        print("test size: {}".format(len(X_test)))
+    start_time = time.monotonic()
+    print("csv read in: {} seconds".format(start_time - csv_start_time))
+
+    sent = ds.process(X_train, y_train, X_test)
+    #print("initReader: {} sent, {} recieved".format(len(inputFile),sent))
+        
+    #print(cppProcess.checkComplete())
+    count =0
+    size = 100
+    x_vec = np.linspace(0,1,size+1)[0:-1]
+    #y_vec = np.random.randn(len(x_vec))
+    #x_vec = np.zeros(shape=(1,1))
+    y_vec = np.zeros(shape=(100,1))
+    line1 = []
+    fig=plt.figure(figsize=(13,6))
+    while ds.checkComplete != True:
+        if config.DEBUG:
+            print('currently processed {} lines...\r'.format(ds.getResultsCount()), end ="")                      
+        #y_vec[-1] = np.random.randn(1)
+        y_vec[-1] = ds.getResultsCount()
+        line1 = live_plotter(x_vec,y_vec,line1, figure=fig)
+        y_vec = np.append(y_vec[1:],0.0)
+        
+        ## display results ##
+        #results = cppProcess.getResults()
+
+
+       
+    ### Results ###
+        
+    results = ds.getResults()
+    end_time = time.monotonic()
+    print("results processed in: {} seconds".format(end_time - start_time))
+    print(type(results))
+    df_results = pd.DataFrame(results)
+    print(df_results.shape)
+    print(y_test.shape)
+
+    # if the y_test array isn't full, initialize it to NAN values so it can be added to the output for consistency sake.
+    if len(y_test) == 0:  
+        y_test = np.full(len(df_results.index), np.nan)
+
+    df_results['label'] = y_test
+    print(df_results.head())
+    df_results = df_results.rename(columns={ df_results.columns[0]: "predicted",df_results.columns[1]: "latency",df_results.columns[2]: "processTime",df_results.columns[3]: "Label"  })
+    #print("return results: {}".format(results))    
+    print("return results: {} rows processed".format(len(df_results)))    
+    print(df_results.head())    
+
+    print(ds.caclulateErr(df_results))
+
+
+    saveResults(df_results)
+    #input()
+
+        
+        
+if __name__ == "__main__":
+    startDataStream()
+    print("program end!")
+    
---
+++ DataStreamer/rtreeBuilder.py
@@ -0,0 +1,188 @@
+'''
+rtreeBuilder.py
+Arguements: -d <datasetFile>, -b <Bvalue>
+Author: Yi Liu  https://github.com/moment-of-peace/python-rtree
+Modified by: Cailen Robertson https://github.com/SanityLacking
+Build a r-tree from a given data set
+'''
+
+# standard libraries
+import getopt
+import sys
+# private libraries
+import Rtree
+#import scanRange
+
+
+class RtreeConstructor:
+
+
+    root = Rtree.Branch
+
+    def __init__(self, *args, **kwargs):       
+        print("Rtree init")
+        #self.Rtree = buildRtree()
+        
+        return super().__init__(*args, **kwargs)
+
+    def fit(self, train, labels):
+
+        return 0 
+
+    def handleOverFlow(self, node):
+        global root
+        global Bvalue
+    
+        # split node into two new nodes
+        nodes = node.split()
+        # if root node is overflow, new root need to build
+        if node.paren == None:
+            self.root = Rtree.Branch(Bvalue, node.level + 1, nodes[0])
+            self.root.addChild(nodes[0])
+            self.root.addChild(nodes[1])
+            self.root.childList[0].paren = self.root
+            self.root.childList[1].paren = self.root
+        else:
+            # update the parent node
+            parent = node.paren
+            parent.childList.remove(node)
+            parent.childList += nodes
+            # check whether parent node is overflow
+            if parent.isOverFlow():
+                handleOverFlow(parent)
+
+    # insert a point to a node
+    def insert(self, node, point):    
+        # if the node is a leaf, add this point
+        if  isinstance(node, Rtree.Leaf):
+            node.addChild(point)
+            if node.isOverFlow():
+                handleOverFlow(node)
+
+        # if the node is a branch, choose a child to add this point
+        elif isinstance(node, Rtree.Branch):
+            node.update(point)
+            childNode = node.chooseChild(point)
+            insert(childNode, point)
+
+        else:
+            pass
+
+    def buildRtree(self, dataset, *B):             
+    
+        Bvalue = 25
+        if len(B) == 1 and B[0] != None:
+            Bvalue = B[0]
+        
+        #f = open(dataSetName, 'rt')
+        #nextLine = f.readline()
+        
+        # size of date set
+        #size = int(nextLine.strip('\n'))
+        # read the first point and build a root
+        #nextLine = f.readline()
+        #point = Rtree.Point(scanRange.getPoint(nextLine))
+        #root = Rtree.Leaf(Bvalue, 1, point)
+        #root.addChild(point)
+    
+        # add the remained points
+        #nextLine = f.readline()
+        for data in dataset:
+            if self.root.size() ==0:
+                point = Rtree.Point(scanRange.getPoint(nextLine))
+                self.root = Rtree.Leaf(Bvalue, 1, point)
+                self.root.addChild(point)
+            #while nextLine != '':
+            point = Rtree.Point(scanRange.getPoint(data))
+            insert(self.root, point)
+            #nextLine = f.readline()
+
+        #f.close()
+        print('R-tree has been built. B is:', Bvalue,'Highest level is:',root.level)
+        return self.root
+
+    # check all nodes and points in a r-tree
+    def checkRtree(self, rtree):    
+        checkBranch(rtree)
+        print('Finished checking R-tree')
+
+    # check the correctness of a leaf node in r-tree
+    def checkLeaf(self, leaf):    
+        # check whether a point is inside of a leaf
+        def insideLeaf(x, y, parent):
+            if x<parent[0] or x>parent[1] or y<parent[2] or y>parent[3]:
+                return False
+            else:
+                return True
+    
+        # general check
+        checkNode(self, leaf)
+        # check whether each child point is inside of leaf's range
+        for point in leaf.childList:
+            if not insideLeaf(point.x, point.y, leaf.range):
+                print('point(', point.x, point.y, 'is not in leaf range:', leaf.range)
+
+    # check the correctness of a branch node in r-tree
+    def checkBranch(self, branch):    
+        # check whether a branch is inside of another branch
+        def insideBranch(child, parent):
+            if child[0]<parent[0] or child[1]>parent[1] or child[2]<parent[2] or child[3]>parent[3]:
+                return False
+            else:
+                return True
+    
+        # general check
+        checkNode(self, branch)
+        # check whether child's range is inside of this node's range
+        for child in branch.childList:
+            if not insideBranch(child.range, branch.range):
+                print('child range:', child.range, 'is not in node range:', branch.range)
+            # check this child
+            if isinstance(child, Rtree.Branch):
+                # if child is still a branch node, check recursively
+                checkBranch(child)
+            elif isinstance(child, Rtree.Leaf):
+                # if child is a leaf node
+                checkLeaf(child)
+
+    # general check for both branch and leaf node
+    def checkNode(self, node):
+        global Bvalue
+    
+        length = len(node.childList)
+        # check whether is empty
+        if length == 0:
+            print('empty node. node level:', node.level, 'node range:', node.range)
+        # check whether overflow
+        if length > Bvalue:
+            print('overflow. node level:', node.level, 'node range:', node.range)
+        
+        # check whether the centre is really in the centre of the node's range
+        r = node.range
+        if (r[0]+r[1])/2 != node.centre[0] or (r[2]+r[3])/2 != node.centre[1]:
+            print('wrong centre. node level:', node.level, 'node range:', node.range)
+        if r[0]>r[1] or r[2]>r[3]:
+            print('wrong range. node level:', node.level, 'node range:', node.range)
+
+
+
+def main():    
+    Tree = R_tree()
+    datasetFile = 'dataset.txt'
+    Bvalue = None
+    
+    ## parse arguments
+    options,args = getopt.getopt(sys.argv[1:],"d:b:")
+    for opt, para in options:
+        if opt == '-d':
+            datasetFile = para
+        if opt == '-b':
+            Bvalue = int(para)
+    
+    # build r-tree from a given data-set
+    Tree.buildRtree(datasetFile, Bvalue)
+    # check the correctness of r-tree
+    Tree.checkRtree(root)
+
+if __name__ == "__main__":
+    main()
